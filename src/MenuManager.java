9256c07bd132b0b71bf7daa36def39bbc9df365c
Jung-Dabin <wjdekqls0020@naver.com>
2022-03-20 20:41:01 +0900

    added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Income");
+		System.out.println("2. Add Spend");
+		System.out.println("3. Edit Details");
+		System.out.println("4. Check Details");
+		System.out.println("5. Exit");
+	}
+
+}
