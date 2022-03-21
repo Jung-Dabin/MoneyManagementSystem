366da434bba0dc1a3dfe33350c02c5c93970a24c
Jung-Dabin <wjdekqls0020@naver.com>
2022-03-21 18:56:08 +0900

    edit part of menu and add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,12 +1,37 @@
+import java.util.Scanner;
 
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Income");
-		System.out.println("2. Add Spend");
-		System.out.println("3. Edit Details");
-		System.out.println("4. Check Details");
-		System.out.println("5. Exit");
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+	
+		while (num != 6) {
+			System.out.println("*** Money Management System Menu ***");
+			System.out.println(" 1. Add Spend");
+			System.out.println(" 2. Delete Spend");
+			System.out.println(" 3. Edit Details");
+			System.out.println(" 4. Check Details");
+			System.out.println(" 5. Show a Menu");
+			System.out.println(" 6. Exit");
+			System.out.println("Select one number between 1-6: ");
+			num = input.nextInt();
+			switch(num) {
+			case 1:
+				System.out.print("Amount of Money: ");
+				int Amount = input.nextInt();
+				System.out.print("Place of Use: ");
+				String Use = input.next();
+				System.out.print("Time: ");
+				String Time = input.next();
+				break;
+			case 2:
+			case 3:
+			case 4:
+				System.out.print("Amount of Money: ");
+				int Amount2 = input.nextInt();
+			}
+		}
 	}
 
 }
